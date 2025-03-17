1ae54bc12e2b22f5171e4340dcc9f13388f76b2f
Mikotaj Inholland <730282@student.inholland.nl>
2025-03-17 15:03:28 +0100

    Remade Project

---
+++ Sommeren/Sommeren/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sommeren.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
